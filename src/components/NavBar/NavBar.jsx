80d70b6119bf7afae23fd32aeeb4a399de2ab644
OrneM <ornemans88@gmail.com>
2025-12-25 21:01:12 -0300

    Implementacion de la NavBar responsive con NavLink para el estilo activo y un layout mobile actualizado

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,11 @@
-
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import CartWidget from "../CartWidget/CartWidget"
 import logo from "../../assets/logo.svg"
+import { useState } from 'react'
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -13,24 +15,25 @@ function NavBar() {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/category/informatica">Informática</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/category/gaming">Gaming</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/category/accesorios">Accesorios</Link>
-                </li>
-              </ul>
-            </div>
           </div>
           <CartWidget />
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Inicio</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/category/informatica">Informática</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/category/gaming">Gaming</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/category/accesorios">Accesorios</NavLink>
+            </li>
+          </ul>
         </div>
 
       </div>
